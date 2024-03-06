d0465e93b7d44141f938401fbad7ab1ba4cf2b0b
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-03-06 11:52:59 +0330

    Add search functionality to partNumberApi.php

--- app/api/partNumberApi.php
+++ app/api/partNumberApi.php
@@ -2,3 +2,32 @@
 require_once '../../config/constants.php';
 require_once '../../database/connect.php';
 require_once '../Middlewares/AuthMiddleware.php';
+
+
+if (isset($_POST['search'])) {
+    $pattern = $_POST['pattern'];
+    // Allow requests from any origin
+    header("Access-Control-Allow-Origin: *");
+
+    // Allow specified HTTP methods
+    header("Access-Control-Allow-Methods:POST");
+
+    // Allow specified headers
+    header("Access-Control-Allow-Headers: Content-Type");
+
+    // Allow credentials (cookies, authorization headers, etc.)
+    header("Access-Control-Allow-Credentials: true");
+
+    // Set content type to JSON
+    header("Content-Type: application/json"); // Allow requests from any origin
+
+    echo json_encode(getMatchedPartNumbers($pattern));
+}
+
+function getMatchedPartNumbers($search)
+{
+    $sql = "SELECT * FROM part_numbers WHERE part_number LIKE '%$search%'";
+    $result = CONN->query($sql);
+    $partNumbers = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $partNumbers;
+}
