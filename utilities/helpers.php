462d83d9d26bb324752fad26ef775112ae9c40bf
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-03-06 11:07:39 +0330

    Remove unnecessary cases from getPageDetails function

--- utilities/helpers.php
+++ utilities/helpers.php
@@ -5,26 +5,8 @@ function getPageDetails($page)
         case 'index.php':
             return ['title' => "مدیریت تلگرام", 'icon' => './public/img/telegram.svg'];
             break;
-        case 'incomplete.php':
-            return ['title' => "ویرایش پیش فاکتور", 'icon' => './public/img/telegram.svg'];
-            break;
-        case 'complete.php':
-            return ['title' => "ویرایش فاکتور", 'icon' => './public/img/telegram.svg'];
-            break;
-        case 'yadakFactor.php':
-            return ['title' => "فاکتور یدک شاپ", 'icon' => './public/img/telegram.svg'];
-            break;
-        case 'insuranceFactor.php':
-            return ['title' => "فاکتور بیمه", 'icon' => './public/img/telegram.svg'];
-            break;
-        case 'partnerFactor.php':
-            return ['title' => "فاکتور همکار", 'icon' => './public/img/telegram.svg'];
-            break;
-        case 'koreaFactor.php':
-            return ['title' => "فاکتور اتوپارت", 'icon' => './public/img/telegram.svg'];
-            break;
         default:
-            return ['title' => "مدیریت فاکتور", 'icon' => './public/img/telegram.svg'];
+            return ['title' => "مدیریت تلگرام", 'icon' => './public/img/telegram.svg'];
             break;
     }
 }
