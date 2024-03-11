11eaa58e2f53daf4e816f6bffbdd65ff9a02da62
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-03-11 15:03:20 +0330

    Add sendMessageWithTemplate function to MessageController.php and remove unnecessary code from test.php

--- test.php
+++ test.php
@@ -4,79 +4,83 @@ require_once './database/connect.php';
 require_once './utilities/PriceHelpers.php';
 require_once './app/Controllers/MessageController.php';
 
-// API endpoint URL
-$apiUrl = 'http://telegram.om-dienstleistungen.de/';
+function boot()
+{
+    // API endpoint URL
+    $apiUrl = 'http://telegram.om-dienstleistungen.de/';
 
-$postData = [
-    'getMessagesAuto' => 'getMessagesAuto'
-];
+    $postData = [
+        'getMessagesAuto' => 'getMessagesAuto'
+    ];
 
-// Initialize curl
-$curl = curl_init();
+    // Initialize curl
+    $curl = curl_init();
 
-// Set the curl options
-curl_setopt_array($curl, [
-    CURLOPT_URL => $apiUrl,
-    CURLOPT_RETURNTRANSFER => true, // Return response as a string instead of outputting it
-    CURLOPT_FOLLOWLOCATION => true, // Follow redirects
-    CURLOPT_MAXREDIRS => 10, // Maximum number of redirects to follow
-    CURLOPT_TIMEOUT => 300, // Timeout in seconds
-    CURLOPT_POST => true, // Set as POST request
-    CURLOPT_POSTFIELDS => http_build_query($postData), // Encode data as URL-encoded format
-    CURLOPT_HTTPHEADER => [ // Optional headers
-        'Content-Type: application/x-www-form-urlencoded',
-    ],
-]);
+    // Set the curl options
+    curl_setopt_array($curl, [
+        CURLOPT_URL => $apiUrl,
+        CURLOPT_RETURNTRANSFER => true, // Return response as a string instead of outputting it
+        CURLOPT_FOLLOWLOCATION => true, // Follow redirects
+        CURLOPT_MAXREDIRS => 10, // Maximum number of redirects to follow
+        CURLOPT_TIMEOUT => 300, // Timeout in seconds
+        CURLOPT_POST => true, // Set as POST request
+        CURLOPT_POSTFIELDS => http_build_query($postData), // Encode data as URL-encoded format
+        CURLOPT_HTTPHEADER => [ // Optional headers
+            'Content-Type: application/x-www-form-urlencoded',
+        ],
+    ]);
 
-// Execute the request
-$response = curl_exec($curl);
+    // Execute the request
+    $response = curl_exec($curl);
 
-// Check for errors
-if (curl_errno($curl)) {
-    $errorMessage = curl_error($curl);
-    // Handle the error
-    echo "cURL error: $errorMessage";
-} else {
-    // Handle the response` 
-    $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE); // Get HTTP status code
-    if ($statusCode >= 200 && $statusCode < 300) {
-        // Request was successful
-        $response = json_decode($response, true);
-        $response = array_values($response);
-
-        validateMessages($response);
+    // Check for errors
+    if (curl_errno($curl)) {
+        $errorMessage = curl_error($curl);
+        // Handle the error
+        echo "cURL error: $errorMessage";
     } else {
-        // Request failed
-        echo "Request failed with status code $statusCode";
-        // You can handle different status codes here
-    }
-}
+        // Handle the response` 
+        $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE); // Get HTTP status code
+        if ($statusCode >= 200 && $statusCode < 300) {
+            // Request was successful
+            $response = json_decode($response, true);
+            $response = array_values($response);
 
-// Close curl
-curl_close($curl);
+            validateMessages($response);
+        } else {
+            // Request failed
+            echo "Request failed with status code $statusCode";
+            // You can handle different status codes here
+        }
+    }
+
+    // Close curl
+    curl_close($curl);
+}
 
 function validateMessages($messages)
 {
     foreach ($messages as $message) {
         $sender = $message['userName'][0];
-
         if (!checkIfValidSender($sender)) {
             echo "Sender $sender is not valid";
             continue;
         }
 
         $messageContent = $message['info'];
-
         $selectedGoods = getSelectedGoods();
 
         foreach ($messageContent as $item) {
-            $codes = explode("\n", $item['code']);
+            $rawCodes = explode("\n", $item['code']);
 
-            $codes = array_filter($codes, function ($line) use ($selectedGoods) {
+            $rawCodes = array_filter($rawCodes, function ($line) use ($selectedGoods) {
+                return $line !== "";
+            });
+
+            $codes = array_filter($rawCodes, function ($line) use ($selectedGoods) {
                 return $line !== "" && in_array($line, $selectedGoods);
             });
             // Now $codes contains the filtered codes
-
             if (count($codes) > 0) {
                 try {
                     $data = getPrice($codes);
@@ -93,6 +97,11 @@ function validateMessages($messages)
                     sendMessageWithTemplate($sender, $template);
                 } catch (Exception $error) {
                     echo 'Error fetching price: ' . $error->getMessage();
+                }
+            } else {
+                if (count($rawCodes) > 0) {
+                    print_r($rawCodes);
+                    echo "کد مدنظر اضافه نشده" . "\n";
                 }
             }
         }
@@ -135,3 +144,5 @@ function getFinalPrice($prices)
 
     return $displayPrices;
 }
+
+boot();
